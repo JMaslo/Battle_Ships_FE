b745ed8e97de6e45d3fb970166d5c840fc132b44
JMaslo <hry.v@post.cz>
2024-01-25 15:52:13 +0100

    Added simple Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div>
-            
+            <p className="text-sky-100 fixed bottom-0 w-full text-center h-16 ">© 2024</p>
         </div>
     )
 }
