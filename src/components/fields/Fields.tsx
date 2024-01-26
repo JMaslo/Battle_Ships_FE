7ddd66498b3441edb635b9bfbbb14e4d4756aa38
JMaslo <hry.v@post.cz>
2024-01-26 13:38:09 +0100

    Added letter and numbers for fields.

--- src/components/fields/Fields.tsx
+++ src/components/fields/Fields.tsx
@@ -1,28 +1,53 @@
+import React from 'react';
+
 export default function Fields() {
-    return (
-        <div className="flex justify-around mt-10">
-            <table className="border-collapse">
-                <tbody>
-                    {Array.from({ length: 10 }).map((_, rowIndex) => (
-                <tr key={rowIndex}>
-                {Array.from({ length: 10 }).map((_, colIndex) => (
-                <td key={colIndex} className=" w-9 h-9 border border-solid border-gray-500 bg-sky-200"></td>
-                ))}
-                </tr>))}
-                </tbody>
-            </table>
-            <p className="text-sky-100 font-medium">Vaše ponořené lodě:</p>
-            <table className="border-collapse">
-                <tbody>
-                    {Array.from({ length: 10 }).map((_, rowIndex) => (
-                <tr key={rowIndex}>
-                {Array.from({ length: 10 }).map((_, colIndex) => (
-                <td key={colIndex} className=" w-9 h-9 border border-solid border-gray-500 bg-sky-200"></td>
-                ))}
-                </tr>))}
-                </tbody>
-            </table>
+  const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+  const numbers = Array.from({ length: 10 }, (_, i) => i + 1);
+
+  const renderLetters = () => {
+    return letters.map((letter, index) => <th key={index}>{letter}</th>);
+  };
+
+  const renderNumbers = () => {
+    return numbers.map((number, index) => <th key={index}>{number}</th>);
+  };
+
+  const renderTableCells = () => {
+    return numbers.map((number) => (
+      <tr key={number}>
+        <th>{number}</th>
+        {letters.map((letter) => (
+          <td key={letter + number} className="w-9 h-9 border border-solid border-gray-500 bg-sky-200"></td>
+        ))}
+      </tr>
+    ));
+  };
+
+  return (
+    <div className="flex justify-around mt-10">
+      <div className="flex">
+        <table className="border-collapse mr-5">
+          <thead>
+            <tr>
+              <th></th>
+              {renderLetters()}
+            </tr>
+          </thead>
+          <tbody>{renderTableCells()}</tbody>
+        </table>
+        <div className="text-sky-100 font-medium text-center mt-5 w-32 mx-auto">
+          Vaše ponořené lodě:
         </div>
-        
-    )
+        <table className="border-collapse">
+          <thead>
+            <tr>
+              <th></th>
+              {renderLetters()}
+            </tr>
+          </thead>
+          <tbody>{renderTableCells()}</tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
